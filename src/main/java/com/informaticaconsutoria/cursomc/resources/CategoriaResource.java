78e6f25f39ffdd89df8f1e6488ccd795e3774d61
Luiz Fernando Justo <fernando.jjusto@gmail.com>
2020-04-21 13:22:23 -0300

    classe de dominio - categoria

--- src/main/java/com/informaticaconsutoria/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/informaticaconsutoria/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.informaticaconsutoria.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.informaticaconsutoria.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@GetMapping
-	public String listar() {
-		return "ok";
+	public List<Categoria> listar() {
+
+		Categoria c1 = new Categoria(1, "TI");
+		Categoria c2 = new Categoria(2, "Desig");
+
+		List<Categoria> list = new ArrayList<>();
+		list.add(c1);
+		list.add(c2);
+
+		return list;
 	}
 
 }
